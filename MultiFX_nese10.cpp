18c4d50858c44730674afe2c5764c1e05e9fd908
NicDox <nicola.domini@live.it>
2022-02-13 18:37:23 +0100

    Started main

--- MultiFX_nese10.cpp
+++ MultiFX_nese10.cpp
@@ -1,10 +1,32 @@
 #include "daisy_seed.h"
 #include "daisysp.h"
 
+// Set max delay time to 0.75 of samplerate.
+#define MAX_DELAY static_cast<size_t>(48000 * 2.5f)
+
 using namespace daisy;
 using namespace daisysp;
 
-DaisySeed hw;
+static DaisySeed hw;
+
+static Phaser										phas;
+static DelayLine<float, MAX_DELAY> DSY_SDRAM_BSS 	dell;
+static DelayLine<float, MAX_DELAY> DSY_SDRAM_BSS 	delr;
+static Tremolo										trem;
+static Parameter deltime;
+
+float currentDelay, feedback, delayTarget, cutoff, dryWet[4];
+bool effectOn[3];
+
+enum FX_types {
+	PHAS,
+	DEL,
+	TREM,
+	// DRY,
+	// ALL,
+};
+
+FX_types which_FX;	//Here is stored the value of the actual effect
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
