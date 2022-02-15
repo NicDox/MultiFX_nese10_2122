14fbe2f11986771451cd82619cd0f822d40dac5c
NicDox <nicola.domini@live.it>
2022-02-15 16:38:09 +0100

    Defined IN-OUT interface

--- Software/MultiFX_nese10.cpp
+++ Software/MultiFX_nese10.cpp
@@ -6,6 +6,13 @@
 
 // Set max delay time to 0.75 of samplerate.
 #define MAX_DELAY static_cast<size_t>(48000 * 2.5f)
+#define PIN_VOL			3		
+#define PIN_FX1			4
+#define PIN_NEXT_FX		14
+#define PIN_BACK_FX		13
+#define PIN_TOGGLE_BP	15
+#define PIN_LED_BP 		17
+
 
 using namespace daisy;
 using namespace daisysp;
@@ -84,10 +91,32 @@ int main(void)
 	del.Init();
 	trem.Init(sr);
 
+	//Configure ADC pins for pots
 	AdcChannelConfig adcConfig;
-	adcConfig.InitSingle(hw.GetPin(21));
+	adcConfig.InitSingle(hw.GetPin(PIN_VOL));	//VOLUME
+	adcConfig.InitSingle(hw.GetPin(PIN_FX1));	//Tune FX_par1
+										//Tune FX_par2 ??
+										//Tune dry/wet percentage ??
 
-	hw.adc.Init(&adcConfig, 1);
+	hw.adc.Init(&adcConfig, 2);
+
+	//Configure GPIO pins for buttons
+	Switch button_BP;
+	Switch button_SW_NEXT;
+	Switch button_SW_BACK;
+
+	const int PB_SR = 1000; //Sample rate to update pushbuttons
+	button_BP.Init(hw.GetPin(PIN_TOGGLE_BP), PB_SR);
+	button_SW_NEXT.Init(hw.GetPin(PIN_NEXT_FX), PB_SR);
+	button_SW_BACK.Init(hw.GetPin(PIN_BACK_FX), PB_SR); 
+
+	dsy_gpio led_BP;
+
+	led_BP.pin = hw.GetPin(PIN_LED_BP);
+	led_BP.mode = DSY_GPIO_MODE_OUTPUT_PP;
+	led_BP.pull = DSY_GPIO_PULLUP;
+	dsy_gpio_init(&led_BP);
+	// led_BP.Init(17, Mode::Input, Pull::PULLUP, Speed::Slow);
 
 	effectOn[0] = effectOn[1] = effectOn[2] = false;
 
